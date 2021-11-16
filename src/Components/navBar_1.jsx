a605b51d2a4215e0df67540feeac7598b35e4020
grooper2 <athanasiou454@gmail.com>
2021-11-16 01:09:56 +0000

    First commit

---
+++ src/Components/navBar_1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const navBar_1 = () =>{
+    return (
+        <div>
+            <nav className="nav_container">
+                <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Feedback</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default navBar_1
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import navBar_1 from '../Components/navBar_1'
+
+const Home = () => {
+    return (
+        <div>
+           <navBar_1/>
+        </div>
+    )
+}
+
+export default Home
